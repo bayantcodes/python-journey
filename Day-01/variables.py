048b3efb26f8f5cc524f23ac7d3f7598683988fa
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-05 23:35:08 +0530

    Create variables.py

---
+++ Day-01/variables.py
@@ -0,0 +1,15 @@
+# =====================================
+# Topic: Variables
+# Author: Bayant Kaur
+# Day: 01
+# Date: 05-07-2026
+# =====================================
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+college = input("Enter your college name: ")
+
+print("\n----- Student Details -----")
+print(f"Name    : {name}")
+print(f"Age     : {age}")
+print(f"College : {college}")
