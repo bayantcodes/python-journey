5d30bbc5f2856b594abfc6bb3caf77c90621e429
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-06 19:26:51 +0530

    Create type_casting.py

---
+++ Day-01/type_casting.py
@@ -0,0 +1,11 @@
+# =====================================
+# Topic: Type Casting
+# Author: Bayant Kaur
+# Day: 01
+# =====================================
+
+number = float(input("Enter a decimal number: "))
+
+print(f"Original Value: {number}")
+print(f"Converted to Integer: {int(number)}")
+print(f"Converted to String: {str(number)}")
