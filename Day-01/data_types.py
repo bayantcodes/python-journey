f56cb579b4b80396037874d9ec6f445318ac0d64
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-06 19:26:01 +0530

    Create data_types.py

---
+++ Day-01/data_types.py
@@ -0,0 +1,15 @@
+# =====================================
+# Topic: Data Types
+# Author: Bayant Kaur
+# Day: 01
+# =====================================
+
+name = "Bayant"
+age = 20
+height = 5.2
+is_student = True
+
+print(f"Name: {name} | Data Type: {type(name)}")
+print(f"Age: {age} | Data Type: {type(age)}")
+print(f"Height: {height} | Data Type: {type(height)}")
+print(f"Student: {is_student} | Data Type: {type(is_student)}")
