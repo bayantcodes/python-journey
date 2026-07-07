9ad8eb0624304f10feb682a11a3aecf9303c30c1
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-07 19:33:55 +0530

    Create string_methods.py

---
+++ Day-02/string_methods.py
@@ -0,0 +1,12 @@
+# =====================================
+# Topic: String Methods
+# Author: Bayant Kaur
+# Day: 02
+# =====================================
+
+name = input("Enter your name: ")
+
+print(f"Upper Case = {name.upper()}")
+print(f"Lower Case = {name.lower()}")
+print(f"Title Case = {name.title()}")
+print(f"Length = {len(name)}")
