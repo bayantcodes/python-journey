fabc01842bd7dfe11d9d48b3b3e8d958f5b0ccc3
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-07 19:36:03 +0530

    Create string_slicing.py

---
+++ Day-02/string_slicing.py
@@ -0,0 +1,15 @@
+# =====================================
+# Topic: String Slicing
+# Author: Bayant Kaur
+# Day: 02
+# =====================================
+
+text = "Machine Learning"
+
+print(f"First Character = {text[0]}")
+print(f"Last Character = {text[-1]}")
+print(f"First 7 Characters = {text[:7]}")
+print(f"Last 8 Characters = {text[-8:]}")
+print(f"Every 2nd Character = {text[::2]}")
+print(f"Every 3rd Character = {text[::3]}")
+print(f"Reverse = {text[::-1]}")
