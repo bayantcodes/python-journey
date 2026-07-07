39cf9f14cb8e7acb76a062511e464adb06be077a
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-07 19:33:12 +0530

    Create operators.py

---
+++ Day-02/operators.py
@@ -0,0 +1,16 @@
+# =====================================
+# Topic: Operators
+# Author: Bayant Kaur
+# Day: 02
+# =====================================
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"Addition = {a + b}")
+print(f"Subtraction = {a - b}")
+print(f"Multiplication = {a * b}")
+print(f"Division = {a / b}")
+print(f"Floor Division = {a // b}")
+print(f"Modulus = {a % b}")
+print(f"Exponent = {a ** b}")
