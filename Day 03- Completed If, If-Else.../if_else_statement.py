57f658280c6d1326ba875377905d8e1487baf026
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-09 22:17:16 +0530

    Create if_else_statement.py

---
+++ Day 03- Completed If, If-Else.../if_else_statement.py
@@ -0,0 +1,20 @@
+# =====================================
+# Topic: If-Else Statement
+# Author: Bayant Kaur
+# Day: 03
+# =====================================
+
+password = input("Enter your password: ")
+
+if password == "python123":
+    print("Login Successful")
+else:
+    print("Wrong Password")
+
+
+shopping_amount = int(input("Enter shopping amount: "))
+
+if shopping_amount >= 1000:
+    print("10% Discount Applied")
+else:
+    print("No Discount")
