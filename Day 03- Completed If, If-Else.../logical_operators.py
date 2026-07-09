4bc11b50727171b575f91f42bd872857262def1b
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-09 22:18:59 +0530

    Create logical_operators.py

---
+++ Day 03- Completed If, If-Else.../logical_operators.py
@@ -0,0 +1,27 @@
+# =====================================
+# Topic: Logical Operators
+# Author: Bayant Kaur
+# Day: 03
+# =====================================
+
+# AND Operator
+bank_balance = 5000
+pin = True
+
+if bank_balance >= 5000 and pin == True:
+    print("Transaction Successful")
+
+
+# OR Operator
+shopping_price = 1000
+premium_member = True
+
+if shopping_price >= 1000 or premium_member == True:
+    print("Discount Applied")
+
+
+# NOT Operator
+wifi = False
+
+if not wifi:
+    print("Connect to WiFi")
