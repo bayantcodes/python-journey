90a5e3801de82d6cfe06cd66832ebf3cc312a5ab
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-09 22:11:16 +0530

    if_statement.py

--- Day 03- Completed If, If-Else.../if_statement.py
+++ Day 03- Completed If, If-Else.../if_statement.py
@@ -1,1 +1,16 @@
+# =====================================
+# Topic: If Statement
+# Author: Bayant Kaur
+# Day: 03
+# =====================================
 
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible for Driving License")
+
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 40:
+    print("Congratulations! You Passed.")
