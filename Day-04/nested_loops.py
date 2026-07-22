408f73137bf1208d998c77a4b1235a99e3c35f3c
Bayant Kaur <kaurbayant12@gmail.com>
2026-07-22 20:01:58 +0530

    nested_loops.py

---
+++ Day-04/nested_loops.py
@@ -0,0 +1,40 @@
+# ====================================
+# Topic : Nested Loops
+# Author: Bayant Kaur
+# Day   : 04
+# ====================================
+
+# Rectangle (5 Rows × 2 Columns)
+print("Rectangle (5 Rows × 2 Columns)")
+for i in range(5):
+    for j in range(2):
+        print("*", end="")
+    print()
+
+# Square (4 × 4)
+print("\nSquare (4 × 4)")
+for i in range(4):
+    for j in range(4):
+        print("*", end="")
+    print()
+
+# Rectangle (5 Rows × 3 Columns)
+print("\nRectangle (5 Rows × 3 Columns)")
+for i in range(5):
+    for j in range(3):
+        print("*", end="")
+    print()
+
+# Rectangle (3 Rows × 6 Columns)
+print("\nRectangle (3 Rows × 6 Columns)")
+for i in range(3):
+    for j in range(6):
+        print("*", end="")
+    print()
+
+# Understanding Rows and Columns
+print("\nRows and Columns Example (3 × 5)")
+for i in range(3):
+    for j in range(5):
+        print("*", end="")
+    print()
