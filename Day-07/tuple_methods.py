0423d08f9fc80595bcfd9030620fc5fecec0a8a3
Bayant Kaur <kaurbayant12@gmail.com>
2026-08-19 01:20:32 +0530

    Create tuple_methods.py

---
+++ Day-07/tuple_methods.py
@@ -0,0 +1,21 @@
+# ===================================
+# Topic : Tuple Methods
+# Author: Bayant Kaur
+# Day   : 07
+# ===================================
+
+
+# count() method
+# Counts how many times an element occurs in a tuple
+
+numbers = (10, 20, 10, 30, 10, 40)
+
+print(numbers.count(10))
+
+
+# index() method
+# Returns the index of the first occurrence of an element
+
+numbers = (10, 20, 30, 40, 20)
+
+print(numbers.index(20))
